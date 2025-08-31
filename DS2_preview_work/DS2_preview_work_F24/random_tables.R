eda6566167728a008b655251cfd8361cdad50f13
Paul Roback <roback@stolaf.edu>
2025-08-31 12:17:23 -0500

    Delete DS2_preview_work directory

--- DS2_preview_work/DS2_preview_work_F24/random_tables.R
+++
@@ -1,7 +0,0 @@
-set.seed(5625)
-students <- c("Rania", "Maroova", "Elise", "Rain", "George", "Gracia",
-              "Anh", "Jackson", "Mario", "Zuzanna", "Theresa", "Z")
-tables <- rep(c("Table 2", "Table 3", "Table 5"), 4)
-tibble(students = students, tables = tables) %>%
-  mutate(tables = sample(tables)) %>%
-  arrange(tables)
